dd916659dee5c0901ed137ef254621a349a525b9
Tatu Arvela <tatu.arvela@wunderdog.fi>
2019-05-24 23:52:52 +0300

    Loading, refactoring, disabled address bar

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,35 @@
 <template>
-  <Browser
-    v-bind:title="title"
-    v-bind:address="address"
-    v-bind:status="status"
-    v-on:page-loaded="updateBrowser"
-  >
-    <CorporationSite v-if="site === 'A'" />
-    <PortalSite v-else-if="site === 'B'" />
-    <RoomSite v-else-if="site === 'C'" />
-    <VaporwaveSite v-else-if="site === 'D'" />
-    <PageNotFound v-else />
-  </Browser>
+  <div id="browser">
+    <p class="ui-text has-icon" id="browser-title">{{ title }}</p>
+    <div class="has-icon" id="browser-addressbar">
+      <input class="ui-text" id="browser-address" v-model="address" />
+    </div>
+    <p class="ui-text has-icon" id="browser-status">{{ status }}</p>
+    <div id="page">
+      <CorporationSite
+        v-if="site === 'A'"
+        :title="title"
+        :address="address"
+        :status="status"
+        @updateBrowser="updateBrowser"
+      />
+      <PortalSite v-else-if="site === 'B'" />
+      <RoomSite v-else-if="site === 'C'" />
+      <VaporwaveSite v-else-if="site === 'D'" />
+      <PageNotFound v-else />
+    </div>
+  </div>
 </template>
 
 <script>
-import Browser from "./components/Browser.vue";
-import CorporationSite from "./components/sites/CorporationSite.vue";
-import PortalSite from "./components/sites/PortalSite.vue";
-import RoomSite from "./components/sites/RoomSite.vue";
-import VaporwaveSite from "./components/sites/VaporwaveSite.vue";
+import CorporationSite from "./components/CorporationSite.vue";
+import PortalSite from "./components/PortalSite.vue";
+import RoomSite from "./components/RoomSite.vue";
+import VaporwaveSite from "./components/VaporwaveSite.vue";
 import PageNotFound from "./components/PageNotFound.vue";
 
 export default {
   components: {
-    Browser,
     CorporationSite,
     PortalSite,
     RoomSite,
@@ -40,6 +46,7 @@ export default {
   },
   methods: {
     updateBrowser(data) {
+      console.log("But can you do this?");
       document.title = data.title;
       this.title = data.title;
       this.address = data.address;
@@ -60,5 +67,145 @@ body
   display: flex
   justify-content: center
   align-items: center
+  overflow: auto
+
+#browser
+  display: block
+  position: relative
+  width: 1024px
+  height: 768px
+  background: url("./assets/browser-border.png")
+  background-repeat: no-repeat
+  background-attachment: fixed
+  background-position: center
+  cursor: url("./assets/cursor.png"), auto
+  animation-name: browser-loading-1
+  animation-duration: 2s
+
+@keyframes browser-loading-1
+  from
+    background-color: teal
+    background-image: none
+  to
+    background-color: teal
+    background-image: none
+
+#browser-title
+  animation-name: browser-loading-2
+  animation-duration: 2.5s
+
+#browser-addressbar
+  animation-name: browser-loading-2
+  animation-duration: 3.5s
+
+#browser-status
+  animation-name: browser-loading-2
+  animation-duration: 3.5s
+
+#page
+  animation-name: browser-loading-2
+  animation-duration: 5s
+
+@keyframes browser-loading-2
+  from
+    opacity: 0
+  to
+    opacity: 0
+
+a
+  cursor: url("./assets/cursor-pointer.png"), auto
+
+input,
+p,
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+li
+  cursor: url("./assets/cursor-text.png"), auto
+
+::selection
+  color: white
+  background: #000080
+
+::-moz-selection
+  color: white
+  background: #000080
+
+.ui-text
+  position: absolute
+  font-family: "Microsoft Sans Serif", sans-serif
+  font-size: 11px
+  margin: 0px
+  cursor: url("./assets/cursor.png"), auto
+  user-select: none
+
+.has-icon:before
+  position: absolute
+  display: block
+  height: 16px
+  width: 16px
+  content: ""
+
+#browser-title
+  font-weight: bold
+  color: white
+  top: 2px
+  left: 21px
+  &:before
+    top: -2px
+    left: -19px
+    background: url("./assets/icon.png")
+
+#browser-addressbar
+  &:after
+    position: absolute
+    display: block
+    height: 16px
+    width: 16px
+    content: ""
+    top: 24px
+    left: 59px
+    background: url("./assets/icon.png")
+  &:before
+    top: 24px
+    right: 13px
+    background: url("./assets/icon-animated.gif")
+  > #browser-address
+    top: 23px
+    left: 59px
+    width: 901px
+    height: 13px
+    user-select: initial
+    padding: 3px
+    padding-left: 18px
+    border: 0px
+    cursor: url("./assets/cursor-text.png"), auto
+    background: #c0c0c0
+    &:focus
+      outline: none
+
+#browser-status
+  height: 1em
+  bottom: 6px
+  left: 22px
+  &:before
+    top: -1px
+    left: -20px
+    background: url("./assets/icon.png")
+
+#page
+  position: absolute
+  display: block
+  background: white
+  font-family: "Microsoft Sans Serif", sans-serif
+  padding: 2px
+  top: 50px
+  left: 2px
+  right: 2px
+  bottom: 23px
+  height: 691px
   overflow: auto
 </style>
